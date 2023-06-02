151196a6e40ceecca7d837c793db873f64feb8ff
BrucelD14 <samuderaduta144@gmail.com>
2023-06-02 22:08:52 +0700

    change color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: "#14b8a6",
+        primary: "#06b6d4",
         secondary: "#64748b",
         dark: "#0f172a",
       },
